04743dd39a3a21ab4fbe0f352e710c2239f38447
MacmillanMatlaila <macmillanmatlaila@gmail.com>
2025-04-07 08:21:42 +0200

    Dart Apprentice Exercise 1

---
+++ Dart apprentice/exercise1.dart
@@ -0,0 +1,10 @@
+import 'dart:math';
+
+void main() {
+  const int myAge = 29;
+  int friendAge = 29;
+  double averageAge;
+  averageAge = 29;
+  averageAge = myAge / friendAge;
+  print(averageAge);
+}
