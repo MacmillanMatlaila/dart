6209a235740758df2d0b03c32b26f4d10e5f14e3
MacmillanMatlaila <macmillanmatlaila@gmail.com>
2025-05-27 23:08:42 +0200

    functions incomplete

---
+++ Dart apprentice/functions.dart
@@ -0,0 +1,7 @@
+/*Note: It’s easy to get the words parameter and argument
+mixed up. A parameter is the name and type that you
+define as an input for your function. An argument, on
+the other hand, is the actual value that you pass in. A
+parameter is abstract, while an argument is concrete.
+*/ 
+
